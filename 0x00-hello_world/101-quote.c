be23cbfa7820599c9352ef015f1685bdb52c72d5
FatmaHashem97 <fatmahashem880@gmail>
2023-06-30 05:02:46 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc -s -masm=intel $CFILE -o `echo $CFILE | cut -d. -f1`.s
+#include <unistd.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: print a qoute using write function 
+ *     ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error).
+ */
+
+
+int main(void)
+{	char quo[] = "and that piece of art is usefule\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
